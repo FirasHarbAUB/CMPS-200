c9f059e3cbb6368a3f26727b8157b2702dae1b3c
Firas Harb <fah41@mail.aub.edu>
2019-01-30 10:20:41 +0200

    Files for assigment1 CMPS 200

---
+++ spring.py
@@ -0,0 +1,12 @@
+import sys
+
+m = int(sys.argv[1])  # Input the 2nd argument in the command line 
+d = int(sys.argv[2])  # Input the 3rd argument in the command line
+
+if (m >= 1 and m <= 12) and (d >= 1 and d <= 31):
+    if (m == 3 and (d >= 21 and d <= 31)) or (m >= 4 and m <= 5) or (m == 6 and (d >=1 and d <= 20)):
+        print(True)
+    else:
+        print(False)
+else:
+    print("Error: Value of Month or Day are out of context. Check Inputs.")
